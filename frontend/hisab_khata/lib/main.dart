aff69ae7e21e18449e836be3c107479063e860e5
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2026-01-18 13:50:14 +0545

    init: firebase setup for FCM

--- frontend/hisab_khata/lib/main.dart
+++ frontend/hisab_khata/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hisab_khata/app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hisab_khata/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
