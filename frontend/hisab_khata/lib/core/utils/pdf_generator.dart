eadac5d52fb4c65df0c4b97c22c00d32810dda36
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2026-03-25 16:06:18 +0545

    fix(pdf): Re-add missing open_file import

--- frontend/hisab_khata/lib/core/utils/pdf_generator.dart
+++ frontend/hisab_khata/lib/core/utils/pdf_generator.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:path_provider/path_provider.dart';
+import 'package:open_file/open_file.dart';
 import 'package:hisab_khata/features/analytics/presentation/bloc/analytics_state.dart';
 import 'package:intl/intl.dart';
 
